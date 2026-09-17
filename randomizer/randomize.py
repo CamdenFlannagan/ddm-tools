a3370572a00d8068d3b4720dc7024fa66e799472
CamdenFlannagan <camdenflannagan@gmail.com>
2026-09-17 18:14:43 -0500

    Improved argparse help messages

--- randomizer/randomize.py
+++ randomizer/randomize.py
@@ -5,8 +5,8 @@ def hex_int(x):
     return int(x, 16)
 
 parser = argparse.ArgumentParser(description="Diggy Diggy Mole Randomizer")
-parser.add_argument("--seed", type=int, default=16, help="Change the seed to generate different maps")
-parser.add_argument("--start", type=hex_int, default=0x77, help="Use hexcode 0xYX to pick a starting room")
+parser.add_argument("--seed", type=int, default=16, help="Change the seed to generate different maps.")
+parser.add_argument("--start", type=hex_int, default=0x77, help="Use hexcode 0xYX to pick a starting room. Y is top to bottom, 0 to F. X is left to right, 0 to F.")
 
 args = parser.parse_args()
 
